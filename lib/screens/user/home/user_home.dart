e4e3566817dbc8da679674b0fb0ab4c72eec7948
gokulhansv <gokulhansv@gmail.com>
2023-02-03 16:03:17 +0530

    new design implemented

--- lib/screens/user/home/user_home.dart
+++ lib/screens/user/home/user_home.dart
@@ -6,6 +6,7 @@ import 'package:outq_new_app/screens/user/components/drawer/user_drawer.dart';
 import 'package:outq_new_app/screens/user/profile/myprofile.dart';
 import 'package:outq_new_app/screens/user/service/user_service_search.dart';
 import 'package:outq_new_app/utils/color_constants.dart';
+import 'package:outq_new_app/utils/sizes.dart';
 
 class UserHomePage extends StatefulWidget {
   const UserHomePage({super.key});
@@ -80,11 +81,66 @@ class UserHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        Center(child: Text("All Services")),
-      ],
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(tDefaultSize),
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 100,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              boxShadow: [
+                BoxShadow(
+                    color: Color.fromRGBO(90, 108, 234, 0.07000000029802322),
+                    offset: Offset(0, 0),
+                    blurRadius: 50)
+              ],
+              color: Colors.green,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                    child:
+                        Image(image: AssetImage('assets/images/Menuphoto.png')),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Herbal Pancake',
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.subtitle2,
+                          ),
+                          Text(
+                            'Herbal Pancake',
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                        ]),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text('\$7',
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.headline2),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
