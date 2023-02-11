86c315c25189aa05e8c3617d9081f5a53df794e6
gokulhansv <gokulhansv@gmail.com>
2023-02-11 13:49:40 +0530

    new

--- lib/Backend/models/owner_models.dart
+++ lib/Backend/models/owner_models.dart
@@ -7,6 +7,14 @@ class StoreModel {
   String name, location, description, type;
   StoreModel(this.name, this.location, this.description, this.type);
 }
+
+
+class ServiceModel {
+  String name, location, description, type;
+  ServiceModel(this.name, this.location, this.description, this.type);
+}
+
+
 class idModel {
   String id;
   idModel(this.id);
--- lib/screens/owner/service/edit/edit_service.dart
+++ lib/screens/owner/service/edit/edit_service.dart
@@ -1,9 +1,13 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:outq_new_app/Backend/models/owner_models.dart';
 import 'package:outq_new_app/screens/owner/components/appbar/owner_appbar.dart';
+import 'package:outq_new_app/screens/owner/store/create/create_store.dart';
 import 'package:outq_new_app/utils/color_constants.dart';
 import 'package:outq_new_app/utils/sizes.dart';
 import 'package:outq_new_app/utils/widget_functions.dart';
+import 'package:http/http.dart' as http;
+
 
 class EditServicePage extends StatefulWidget {
   const EditServicePage({super.key});
@@ -86,7 +90,36 @@ class EditServiceForm extends StatefulWidget {
   State<EditServiceForm> createState() => _EditServiceFormState();
 }
 
+TextEditingController nameController = TextEditingController(text: '');
+TextEditingController typeController = TextEditingController(text: '');
+TextEditingController descriptionController = TextEditingController(text: '');
+TextEditingController locationController = TextEditingController(text: '');
+
+ServiceModel service = ServiceModel('', '', '', '');
+
 class _EditServiceFormState extends State<EditServiceForm> {
+  Future save() async {
+    print({service.name, service.type, service.description, service.location});
+    http.post(
+        Uri.parse(
+          "http://192.168.137.1:3001/auth/owner/register",
+        ),
+        headers: <String, String>{
+          'Context-Type': 'application/json; charset=UTF-8',
+        },
+        body: <String, String>{
+          'name': service.name,
+          'location': service.location,
+          'description': service.description,
+          'type': service.type,
+        });
+    // Get.to(() => {const OwnerHomePage()});
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+            builder: (BuildContext context) => const CreateStorePage()),
+        (Route<dynamic> route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
@@ -106,15 +139,19 @@ class _EditServiceFormState extends State<EditServiceForm> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(22),
                 ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Shop Name',
+                child: TextField(
+                  controller: nameController,
+                  onChanged: (val) {
+                    service.name = val;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'service Name',
                     labelStyle: TextStyle(
                       fontFamily: 'Montserrat',
                       fontWeight: FontWeight.bold,
                       color: Colors.grey,
                     ),
-                    // hintText: 'myshop..',
+                    // hintText: 'myservice..',
                   ),
                 ),
               ),
@@ -125,15 +162,19 @@ class _EditServiceFormState extends State<EditServiceForm> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(22),
                 ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Shop Name',
+                child: TextField(
+                  controller: locationController,
+                  onChanged: (val) {
+                    service.location = val;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Location',
                     labelStyle: TextStyle(
                       fontFamily: 'Montserrat',
                       fontWeight: FontWeight.bold,
                       color: Colors.grey,
                     ),
-                    // hintText: 'myshop..',
+                    // hintText: 'myservice..',
                   ),
                 ),
               ),
@@ -144,15 +185,19 @@ class _EditServiceFormState extends State<EditServiceForm> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(22),
                 ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Shop Name',
+                child: TextField(
+                  controller: descriptionController,
+                  onChanged: (val) {
+                    service.description = val;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Description',
                     labelStyle: TextStyle(
                       fontFamily: 'Montserrat',
                       fontWeight: FontWeight.bold,
                       color: Colors.grey,
                     ),
-                    // hintText: 'myshop..',
+                    // hintText: 'myservice..',
                   ),
                 ),
               ),
@@ -163,15 +208,19 @@ class _EditServiceFormState extends State<EditServiceForm> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(22),
                 ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Shop Name',
+                child: TextField(
+                  controller: typeController,
+                  onChanged: (val) {
+                    service.type = val;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Type',
                     labelStyle: TextStyle(
                       fontFamily: 'Montserrat',
                       fontWeight: FontWeight.bold,
                       color: Colors.grey,
                     ),
-                    // hintText: 'myshop..',
+                    // hintText: 'myservice..',
                   ),
                 ),
               ),
